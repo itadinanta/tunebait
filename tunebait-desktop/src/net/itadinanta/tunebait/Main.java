5eb0a54e5f7447c631be209ffdba2b391b89748d
Nicola Orrù <nigu.orru@gmail.com>
2012-08-28 18:02:47 +0100

    Physics working

--- tunebait-desktop/src/net/itadinanta/tunebait/Main.java
+++ tunebait-desktop/src/net/itadinanta/tunebait/Main.java
@@ -4,9 +4,11 @@ import net.itadinanta.tunebait.TunebaitGame;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.utils.GdxNativesLoader;
 
 public class Main {
 	public static void main(String[] args) {
+		GdxNativesLoader.load(); 
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "tunebait";
 		cfg.useGL20 = true;
