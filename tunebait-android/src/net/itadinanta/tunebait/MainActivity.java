fb0d2af5ac11765f8b1714ce39609fe4813c14fe
Nicola Orrù <nigu.orru@gmail.com>
2012-08-28 17:13:08 +0100

    Initial import, working Android app

---
+++ tunebait-android/src/net/itadinanta/tunebait/MainActivity.java
@@ -0,0 +1,19 @@
+package net.itadinanta.tunebait;
+
+import net.itadinanta.tunebait.TunebaitGame;
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new TunebaitGame(), cfg);
+    }
+}
